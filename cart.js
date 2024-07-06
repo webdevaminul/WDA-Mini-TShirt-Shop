811ab823e7184c8cc2e5abfda489ce7c9d226213
webdevaminul <webdev.aminul@gmail.com>
2024-07-06 23:54:56 +0600

    Toggle Cart Icon

---
+++ cart.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+const cartIcon = document.querySelector(".cartIcon");
+const closeBtn = document.querySelector(".cartBtns .cartClose");
+
+// ToggleCart
+cartIcon.addEventListener("click", () => {
+  body.classList.toggle("activeCart");
+});
+closeBtn.addEventListener("click", () => {
+  body.classList.toggle("activeCart");
+});

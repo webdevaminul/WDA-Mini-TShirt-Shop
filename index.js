597443c8f533242b543a24bcf7dbbcb03b850949
webdevaminul <webdev.aminul@gmail.com>
2024-07-09 20:50:38 +0600

    Loading Index.html's main to template

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import cart from "./cart.js";
-const app = document.getElementById("app");
 
+const app = document.getElementById("app");
+const temporatyContent = document.getElementById("temporatyContent");
+
+// Load template and apply cart functionality to the main content section
 const loadTemplate = () => {
   fetch("template.html")
     .then((response) => response.text())
     .then((value) => {
-      app.innerHTML = value;
-
+      app.innerHTML = value; //Load the entire template to index.html
+      const mainContent = document.getElementById("mainContent"); //Load index.html main to template.html main
+      mainContent.innerHTML = temporatyContent.innerHTML;
       cart();
     });
 };

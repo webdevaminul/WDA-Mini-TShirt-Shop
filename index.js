21f10923eb92d706bf7861b5042b951fce835ec7
webdevaminul <webdev.aminul@gmail.com>
2024-07-10 10:04:32 +0600

    Product showed in Index Page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import cart from "./cart.js";
+import products from "./products.js";
 
 const app = document.getElementById("app");
 const temporatyContent = document.getElementById("temporatyContent");
@@ -6,17 +7,32 @@ const temporatyContent = document.getElementById("temporatyContent");
 // Load template and apply cart functionality to the main content section
 const loadTemplate = () => {
   fetch("template.html")
-    .then((response) => response.text())
+    .then((response) => response.text()) //Convert html page to text format
     .then((value) => {
       app.innerHTML = value; //Load the entire template to index.html
-      const mainContent = document.getElementById("mainContent"); //Load index.html main to template.html main
-      mainContent.innerHTML = temporatyContent.innerHTML;
-      temporatyContent.innerHTML = null; //Delete temporary content to stop 2 content
-      cart();
+      const mainContent = document.getElementById("mainContent");
+      mainContent.innerHTML = temporatyContent.innerHTML; //Load index.html main to template.html main
+      temporatyContent.innerHTML = null; //Delete temporary content to stop showing 2 content
+      cart(); //Run cart functionality in Index Page
+      initApp(); //Run product list functionality in Index Page
     });
 };
 loadTemplate();
 
 const initApp = () => {
   //Load Product List
+  const productsContainer = document.querySelector(".productsContainer");
+  productsContainer.innerHTML = null; //Products Container null before setting products
+  products.forEach((product) => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+    productElement.innerHTML = `
+      <img src="${product.image}"/>
+      <h2>${product.name}</h2>
+      <p>${product.price}</p>
+      <button>Add to Cart</button>
+      `;
+    productsContainer.appendChild(productElement);
+  });
+  console.log(productsContainer);
 };

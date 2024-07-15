5521401a2dc170af884767756ecce80f54dc25c6
webdevaminul <webdev.aminul@gmail.com>
2024-07-16 01:28:46 +0600

    Added Similar Products

--- javascript/detail.js
+++ javascript/detail.js
@@ -7,23 +7,55 @@ const temporaryContent = document.getElementById("temporaryContent");
 
 loadHTML("template.html", app).then(() => {
   const mainContent = document.getElementById("mainContent");
-  mainContent.innerHTML = temporaryContent.innerHTML; //Load index.html main to template.html main
-  temporaryContent.innerHTML = null; //Delete temporary content to stop showing 2 content
-  cart(); //Run cart functionality in Index Page
-  initApp(); //Run product list functionality in Index Page
+  mainContent.innerHTML = temporaryContent.innerHTML; // Load index.html main to template.html main
+  temporaryContent.innerHTML = null; // Delete temporary content to stop showing 2 content
+  cart(); // Run cart functionality in Index Page
+  initApp(); // Run product list functionality in Index Page
 });
 
 const initApp = () => {
-  const paramsID = new URLSearchParams(window.location.search).get("id");
-  const info = products.filter((product) => product.id == paramsID)[0];
+  const paramsID = new URLSearchParams(window.location.search).get("id"); // Get the params id
+  const info = products.filter((product) => product.id == paramsID)[0]; // Get the product info by params id
   if (!info) {
-    window.location.href = "/";
+    window.location.href = "/"; // Rederect user to home if params id doesn't match
   }
-
+  // Show product details in the page
   const detailsContainer = document.querySelector(".detailsContainer");
-  detailsContainer.querySelector(".image img").src = info.image;
-  detailsContainer.querySelector(".detailTitle").innerText = info.name;
-  detailsContainer.querySelector(".detailPrice").innerText = info.price;
-  detailsContainer.querySelector(".description").innerText = info.description;
-  detailsContainer.querySelector(".addCart").dataset.id = info.id;
+  detailsContainer.innerHTML = `
+  <figure class="image">
+    <img src="${info.image}" alt="Product Image" />
+  </figure>
+  <div class="detailsContent">
+    <h1 class="detailTitle">${info.name}</h1>
+    <p class="detailPrice">$${info.price}</p>
+    <div class="detailBtns">
+      <button>Check Out</button>
+      <button class="addCart" data-id=${info.id}>
+        Add to Cart
+      </button>
+    </div>
+    <div class="description">${info.description}</div>
+  </div>
+
+  <h1 class="titleSimilar">Similar Products</h1>
+  `;
+
+  const productsContainer = document.querySelector(".productsContainer");
+  // productsContainer.innerHTML = null; //Products Container null before setting products
+
+  products
+    .filter((value) => value.id != paramsID)
+    .forEach((product) => {
+      const productElement = document.createElement("div"); //Create product element
+      productElement.classList.add("product");
+      productElement.innerHTML = `
+      <a href="/detail.html?id=${product.id}">
+        <img src="${product.image}"/>
+      </a>
+      <h2>${product.name}</h2>
+      <p>$${product.price}</p>
+      <button class="addCart" data-id="${product.id}">Add to Cart</button> 
+      `;
+      productsContainer.appendChild(productElement); //insert product element to productsContainer
+    });
 };
